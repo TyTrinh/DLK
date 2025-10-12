60cb0281c74beb71944b5f9f87f403878e4d4e5a
TyTrinh <trinhtran180206@gmail.com>
2025-10-12 16:08:40 +0700

    Update Admin.h

--- SFMLapp/include/Admin.h
+++ SFMLapp/include/Admin.h
@@ -1,12 +1,31 @@
 #pragma once
-#include "User.h"
+#include "Specialization.h"
+// #include "Doctor.h"
+// #include "Patient.h"
+using namespace std;
 
+class Admin{
+    private:
+        string adminID;
+        int adminPass;
 
-class Admin : public User {
-public:
-Admin() = default;
-Admin(std::string id, std::string name) : User(std::move(id), std::move(name)) {}
-
-
-// admin-specific methods can be added later
+    public:
+        void adjustSchedule(); // Điều chỉnh lịch khám
+        void quantityOfBookedManager(); // Quản lý số lượng đặt khám
+        void cancelSchedule(); // Hủy lịch khám trong các trường hợp: bệnh nhân đk nhiều lần trên tuần, đk bị lặp
+        void patientManager();
+        void doctorManager(Specialization& spe, Doctor& doctor);
+        void exportManager(); // Thống kê, báo cáo
+    // ----------Các hàm có trong doctorManager()-------------- (và tương tự đối với patientManager())
+        // Copy constructor
+        // Doctor(const Doctor& other);
+        // // Toán tử gán sao chép
+        // Doctor& operator=(const Doctor& other); // đổi từ bác sĩ này sang bác sĩ khác
+        // // Move constructor
+        // Doctor (Doctor&& other) noexcept; // Chuyển giao quyền sở hữu tài nguyên
+        // // Toán tử gán di chuyển
+        // Doctor& operator=(Doctor&& other) noexcept;
 };
+
+// tạo sẵn acc cho admin
+// từng file dữ liệu 0-100; 101-200
