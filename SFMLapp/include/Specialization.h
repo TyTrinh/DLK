678379308203a2fca919d2e9a98f9de607ce539d
TyTrinh <trinhtran180206@gmail.com>
2025-10-12 16:07:06 +0700

    Add files via upload

---
+++ SFMLapp/include/Specialization.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <fstream>
+#include <filesystem>
+// #include <map>
+#include "Doctor.h"
+using namespace std;
+
+
+class Specialization{
+    public:
+        string speID;
+        string speName;
+        map<string,Doctor> doctorList;
+
+        Specialization();
+        Specialization(string speID, string speName);
+        ~Specialization();
+
+        // Move constructor
+        //Specialization(Specialization&& other) noexcept;
+
+        void setSpecialization(string speID);
+        string getSpecializationID();
+        void setSpecializationName(string speName);
+        string getSpecializationName();
+
+        
+        bool addDoctor(Doctor& doctor);
+        // bool removeDoctor(Doctor& doctor);
+        // bool listDoctor();
+};
